aff5830f7b747ee9261c13935b139568c4b4d989
yh <devnull@localhost>
2011-02-11 18:48:07 -0800

    class MPI4pywrapper(MPIwrapper): 	""" 	2011 		a derivative of MPIwrapper, for mpi4py
    			1. no more pickling
    			2. recv() replaces receiveString()
    
    				syntax difference
    
    				to receive something from anywhere in mpi4py:
    					status = MPI.Status()
    					any_src, any_tag = MPI.ANY_SOURCE, MPI.ANY_TAG
    					data = communicator.recv(source=any_src, tag=any_tag, status=status)
    					source = status.source
    
    	"""

--- MPIwrapper.py
+++ MPIwrapper.py
@@ -233,7 +233,7 @@ class MPIwrapper(object):
 				data, source, tag = communicator.receiveString(None, 1)
 			else:
 				data, source, tag, count = communicator.receive(type, None, 1)
-		sys.stderr.write("Node no.%s output done.\n"%node_rank)		
+		sys.stderr.write("Node no.%s output done.\n"%node_rank)
 	
 	def input_node(self, curs, free_computing_nodes, message_size=None, input_handler=fetch_cluster_block):
 		"""
@@ -368,3 +368,168 @@ class MPIwrapper(object):
 			data, source, tag = communicator.receiveString(inputNodeRank, 0)	#get data from node 0
 		if not cleanup_handler:
 			self.computing_cleanup_handler(communicator, output_node_rank=output_node_rank)
+
+class MPI4pywrapper(MPIwrapper):
+	"""
+	2011
+		a derivative of MPIwrapper, for mpi4py
+			
+			1. no more pickling
+			2. recv() replaces receiveString()
+			
+				syntax difference
+			
+				to receive something from anywhere in mpi4py:
+					status = MPI.Status()
+					any_src, any_tag = MPI.ANY_SOURCE, MPI.ANY_TAG
+					data = communicator.recv(source=any_src, tag=any_tag, status=status)
+					source = status.source
+		
+	"""
+	def __init__(self, communicator, debug=0, report=0):
+		MPIwrapper.__init__(self, communicator, debug=debug, report=report)
+	
+	def inputNode(self, param_obj, free_computing_nodes, param_generator=None, message_size=1):
+		"""
+		2011-2-10
+			derived from MPIwrapper.inputNode
+			
+			1. no more pickling
+			2. recv() replaces receiveString()
+		"""
+		communicator = self.communicator
+		node_rank = communicator.rank
+		sys.stderr.write("Input node(%s) working...\n"%node_rank)
+		counter = 0
+		timeToBreak = 0
+		generatorMethod = 'next'
+		if not hasattr(param_generator, generatorMethod) and hasattr(param_generator, 'pop'):
+			generatorMethod = 'pop'
+		while 1:
+			param_ls = []
+			for i in range(message_size):
+				try:
+					param = getattr(param_generator, generatorMethod)()
+					param_ls.append(param)
+				except:
+					if self.report:
+						sys.stderr.write('Except type: %s\n'%repr(sys.exc_info()))
+						import traceback
+						traceback.print_exc()
+						sys.stderr.write('Input node broke out due to except.\n')
+					timeToBreak = 1
+					break
+			if len(param_ls)>0:
+				if self.report:
+					sys.stderr.write('Requesting free node from %s \n'%param_obj.output_node_rank)
+				communicator.send("1", param_obj.output_node_rank, 1)	#WATCH: tag is 1, to the output_node.
+				free_computing_node = communicator.recv(source=param_obj.output_node_rank, tag=2)
+				if self.report:
+					sys.stderr.write('node %s is free \n'%free_computing_node)
+					#WATCH: tag is 2, from the output_node
+				#data_pickle = cPickle.dumps(param_ls, -1)
+				communicator.send(param_ls, int(free_computing_node), 0)	#WATCH: int()
+				if self.report:
+					sys.stderr.write("block %s sent to %s.\n"%(counter, free_computing_node))
+				counter += 1
+			if timeToBreak:
+				break
+			
+		#tell computing_node to exit the loop
+		for node in free_computing_nodes:	#send it to the computing_node
+			if self.report:
+				sys.stderr.write("exit signal sent to %s.\n"%(node))
+			communicator.send("-1", node, 0)
+		sys.stderr.write("Input node(%s) done\n"%(node_rank))
+	
+	
+	def computing_node(self, parameter_list, node_fire_handler, cleanup_handler=None, output_node_rank=None, inputNodeRank=None):
+		"""
+		2011-2-10
+			1. no more pickling
+			2. recv() replaces receiveString()
+		"""
+		report = self.report
+		communicator = self.communicator
+		node_rank = communicator.rank
+		if inputNodeRank is None:
+			inputNodeRank = 0
+		data = communicator.recv(source=inputNodeRank, tag=0)	#get data from node 0
+		if output_node_rank is None:
+			output_node_rank = communicator.size-1
+		while 1:
+			if data=="-1":
+				if self.report:
+					sys.stderr.write("node %s broke.\n"%node_rank)
+				break
+			else:
+				result = node_fire_handler(communicator, data, parameter_list)
+				if self.report:
+					sys.stderr.write("node %s is pickling result ...\n"%node_rank)
+				#result_pickle = cPickle.dumps(result, -1)
+				#del result
+				if self.report:
+					sys.stderr.write("node %s result-pickling done and sending it to node %s.\n"%(node_rank, output_node_rank))
+				communicator.send(result, output_node_rank, 1)
+				#del result_pickle
+				if self.report:
+					sys.stderr.write("node %s result-sending done.\n"%(node_rank))
+			data = communicator.recv(source=inputNodeRank, tag=0)	#get data from node 0
+		if not cleanup_handler:
+			self.computing_cleanup_handler(communicator, output_node_rank=output_node_rank)
+	
+	def output_node(self, free_computing_nodes, parameter_list, handler, type=1):
+		"""
+		2011-2-10
+			1. no more pickling
+			2. recv() replaces receiveString()
+		"""
+		communicator = self.communicator
+		report = self.report
+		node_rank = communicator.rank
+		from mpi4py import MPI		
+		status = MPI.Status()
+		any_src, any_tag = MPI.ANY_SOURCE, MPI.ANY_TAG
+		sys.stderr.write("Node no.%s ready to accept output...\n"%node_rank)
+		data = communicator.recv(source=any_src, tag=any_tag, status=status)
+		source = status.source
+		no_of_computing_nodes = len(free_computing_nodes)
+		no_of_resting_nodes = 0	#to keep track how many computing_nodes have rested
+		free_computing_node_request = 0	#10-21-05 Flag used to check whether node 0 is requesting
+		request_node = 0	#default request_node is 0
+		while 1:
+			if source==0:	#10-19-05 the input_node is asking me for free computing_node WATCH: it's array
+				if self.report:
+					sys.stderr.write("node %s request a free computing node.\n"%(source,))
+				if len(free_computing_nodes)==0:	#10-21-05, it's empty. Flag the request.
+					if self.report:
+						sys.stderr.write("no free computing node available now.\n")
+					free_computing_node_request = 1
+					request_node = source	#record the request_node
+				else:
+					free_computing_node = free_computing_nodes.pop(0)
+					if self.report:
+						sys.stderr.write("free computing node %s is  available.\n"%(free_computing_node))
+					communicator.send(str(free_computing_node), dest=source, tag=2)	#WATCH tag is 2.
+			elif data=="-1":
+				no_of_resting_nodes += 1
+				if self.report:
+					sys.stderr.write("node %s(%s-th) rested.\n"%(source, no_of_resting_nodes))
+				if no_of_resting_nodes==no_of_computing_nodes:	#WATCH: its' size-3
+					break
+					if self.report:
+						sys.stderr.write("node %s output finished.\n"%node_rank)
+			else:
+				if free_computing_node_request==1:	#there's an old request for free_computing_node.
+					if self.report:
+						sys.stderr.write("send the newly-free node %s to node %s.\n"%(source, request_node))
+					communicator.send(str(source), dest=request_node, 2)	#WATCH tag is 2.
+					free_computing_node_request = 0
+				else:
+					if self.report:
+						sys.stderr.write("node %s has finished its job.\n"%(source,))
+					free_computing_nodes.append(source)	#append the free computing_node
+				handler(communicator, parameter_list, data)
+			data = communicator.recv(source=any_src, tag=any_tag, status=status)
+			source = status.source
+		sys.stderr.write("Node no.%s output done.\n"%node_rank)		
