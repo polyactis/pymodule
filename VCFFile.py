5e27fc779d00f08801e13fa6efe9a63b92759b37
yuhuang <yuhuang@usc.edu>
2012-01-17 17:25:13 -0800

    def parseOneVCFRow(row, col_name2index, col_index_individual_name_ls, sample_id2index, minDepth=1,\ 				dataEntryType=1): 	""" 	2012.1.17 		common snippet split out of VCFFile & VCFRecord 		row is a list of input columns from one VCF file line 		dataEntryType 			1: each cell is base call 			2: each cell is a dictionary {'GT': base-call, 'DP': depth}

--- VCFFile.py
+++ VCFFile.py
@@ -5,7 +5,7 @@ Example:
 	vcfFile = VCFFile(inputFname=self.inputFname)
 	trio_col_index_data = self.findTrioIndex(vcfFile.sample_id2index, self.trio_isq_id_ls)
 	for vcfRecord in vcfFile.parseIter():
-		locus = vcfRecord.locus
+		locus_id = vcfRecord.locus_id
 		chr = vcfRecord.chr
 		pos = vcfRecord.pos
 		pos = int(pos)
@@ -21,6 +21,137 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from ProcessOptions import  ProcessOptions
 from utils import dict_map, importNumericArray, figureOutDelimiter, PassingData, getColName2IndexFromHeader
 import copy, csv
+import re
+#".|.", "0|1" for phased 
+#"./.", "0/1" for unphased
+diploidGenotypePattern = re.compile(r'([\d.])[|/]([\d.])')
+
+def parseOneVCFRow(row, col_name2index, col_index_individual_name_ls, sample_id2index, minDepth=1,\
+				dataEntryType=1):
+	"""
+	2012.1.17
+		common snippet split out of VCFFile & VCFRecord
+		row is a list of input columns from one VCF file line
+		dataEntryType
+			1: each cell is base call
+			2: each cell is a dictionary {'GT': base-call, 'DP': depth}
+	"""
+	chr = row[0]
+	pos = row[1]
+	quality = row[5]
+	
+	info = row[7]
+	info_ls = info.split(';')
+	info_tag2value = {}
+	for info in info_ls:
+		try:
+			tag, value = info.split('=')
+		except:
+			#sys.stderr.write("Error in splitting %s by =.\n"%info)	###Error in splitting DS by =.
+			continue
+		info_tag2value[tag] = value
+	
+	locus_id = (chr, pos)
+	refBase = row[col_name2index['REF']]
+	altBase = row[col_name2index['ALT']]
+	
+	altBaseLs = altBase.split(',')	#altBase could be just "C" or "C,G" (multi-nucleotide)
+	alleleLs = [refBase] + altBaseLs
+	alleleNumber2Base = {'.':'NA'}
+	for i in xrange(len(alleleLs)):
+		alleleNumber2Base[repr(i)] = alleleLs[i]
+	
+	format_column = row[col_name2index['FORMAT']]
+	format_column_ls = format_column.split(':')
+	format_column_name2index = getColName2IndexFromHeader(format_column_ls)
+		
+	if dataEntryType==1:
+		data_row = ['NA']*(len(col_index_individual_name_ls)+1)	# extra 1 for the ref
+		data_row[0] = refBase
+	else:
+		data_row = [None]*(len(col_index_individual_name_ls)+1)	# extra 1 for the ref
+		data_row[0] = {'GT':refBase, 'DP':-1}
+	genotypeCall2Count = {}
+	for individual_col_index, individual_name in col_index_individual_name_ls:
+		individual_name = individual_name
+		if individual_name not in sample_id2index:
+			sample_id2index[individual_name] = len(sample_id2index)
+		
+		#coverage = read_group2coverage[individual_name]
+		genotype_data = row[individual_col_index]
+		genotype_data_ls = genotype_data.split(':')
+		genotype_call_index = format_column_name2index.get('GT')
+		genotype_quality_index = format_column_name2index.get('GQ')
+		if genotype_quality_index is None:
+			genotype_quality_index = format_column_name2index.get('DP')
+		depth_index = format_column_name2index.get("DP")
+		#GL_index = format_column_name2index.get('GL')
+		if len(genotype_data_ls)<len(format_column_name2index):	#this genotype call is probably empty "./." due to no reads
+			continue
+		if depth_index is None or genotype_call_index is None:
+			sys.stderr.write("")
+			continue
+		#genotype_quality = genotype_data_ls[genotype_quality_index]
+		genotype_call = genotype_data_ls[genotype_call_index]
+		if genotype_call=='./.':
+			continue
+		depth = genotype_data_ls[depth_index]
+		if depth=='.':	#this means depth=0
+			depth = 0
+		else:
+			depth = int(depth)
+		if depth<minDepth:	#no read. samtools would still assign ref/ref to this individual
+			continue
+		#if depth>maxNoOfReads*coverage or depth<minNoOfReads*coverage:	#2011-3-29 skip. coverage too high or too low
+		#	continue
+		genotypeCallInBase = 'NA'
+		callData = {'DP':depth}
+		patternSearchResult = diploidGenotypePattern.search(genotype_call)
+		if patternSearchResult:
+			allele1 = alleleNumber2Base[patternSearchResult.group(1)]
+			allele2 = alleleNumber2Base[patternSearchResult.group(2)]
+			if allele1!='N' and allele2!='N':
+				genotypeCallInBase = '%s%s'%(allele1, allele2)
+		"""
+		if genotype_call=='0/1' or genotype_call =='1/0':	#heterozygous, the latter notation is never used though.
+			allele = '%s%s'%(refBase, altBase)
+			GL_list = genotype_data_ls[GL_index]
+			GL_list = GL_list.split(',')
+			GL_list = map(float, GL_list)
+			GL = GL_list[1]
+			sndHighestGL = max([GL_list[0], GL_list[2]])
+			deltaGL = GL-sndHighestGL
+			
+			AD = genotype_data_ls[format_column_name2index.get('AD')]
+			AD = map(int, AD.split(','))
+			minorAlleleCoverage = min(AD)
+			majorAlleleCoverage = max(AD)
+			
+			if minorAlleleCoverage<=minorAlleleDepthUpperBoundCoeff*coverage and \
+					minorAlleleCoverage>=minorAlleleDepthLowerBoundCoeff*coverage and \
+					majorAlleleCoverage<=majorAlleleDepthUpperBoundCoeff*coverage:
+				DP4_ratio = float(AD[0])/AD[1]
+				allele = '%s%s'%(refBase, altBase)
+
+		elif genotype_call=='./.' or genotype_call=='.|.':	#missing
+			allele = 'NA'
+		elif genotype_call =='1/1' or genotype_call =='1|1':
+			allele = '%s%s'%(altBase, altBase)
+		elif genotype_call =='0/0' or genotype_call=='0|0':
+			allele = '%s%s'%(refBase, refBase)
+		"""
+		col_index = sample_id2index.get(individual_name)
+		if dataEntryType==1:
+			data_row[col_index] = genotypeCallInBase
+		else:
+			callData['GT'] = genotypeCallInBase
+			data_row[col_index] = callData
+		if genotypeCallInBase!='NA':
+			if genotypeCallInBase not in genotypeCall2Count:
+				genotypeCall2Count[genotypeCallInBase] = 0
+			genotypeCall2Count[genotypeCallInBase] += 1
+	return PassingData(chr=chr, pos=pos, locus_id=locus_id, quality=quality, info_tag2value=info_tag2value, refBase=refBase, altBase=altBase, \
+			alleleLs=alleleLs, alleleNumber2Base=alleleNumber2Base, genotypeCall2Count=genotypeCall2Count, data_row=data_row)
 
 class VCFRecord(object):
 	"""
@@ -28,109 +159,46 @@ class VCFRecord(object):
 		
 	"""
 	def __init__(self, row, col_name2index=None, individual_name2col_index=None, sample_id2index=None,\
-				col_index_individual_name_ls=None, **keywords):
+				col_index_individual_name_ls=None, minDepth=1, **keywords):
 		"""
+		2012.1.17
+			add argument minDepth
 		"""
 		self.col_name2index = col_name2index
 		self.individual_name2col_index = individual_name2col_index
 		self.sample_id2index = sample_id2index
 		self.col_index_individual_name_ls = col_index_individual_name_ls
+		self.minDepth = minDepth
 		self.data_row = []
 		self._parse(row)
-	
+		self.alleleLs = []	#index 0 is refBase, 1 is first altBase, 2 is 2nd altBase .. 
+		self.alleleNumber2Base = {}	#map string type of allele 0,1,2 to the actual base
+		
 	def _parse(self, row):
-		chr = row[0]
-		pos = row[1]
-		quality = row[5]
+		returnData = parseOneVCFRow(row, self.col_name2index, self.col_index_individual_name_ls, self.sample_id2index, \
+					minDepth=self.minDepth, dataEntryType=2)
 		
-		info = row[7]
-		info_ls = info.split(';')
-		info_tag2value = {}
-		for info in info_ls:
-			try:
-				tag, value = info.split('=')
-			except:
-				#sys.stderr.write("Error in splitting %s by =.\n"%info)	###Error in splitting DS by =.
-				continue
-			info_tag2value[tag] = value
-		
-		locus = '%s_%s'%(chr, pos)
-		self.refBase = refBase = row[self.col_name2index['REF']]
-		self.altBase = altBase = row[self.col_name2index['ALT']]
-		
-		format_column = row[self.col_name2index['FORMAT']]
-		format_column_ls = format_column.split(':')
-		format_column_name2index = getColName2IndexFromHeader(format_column_ls)
-		data_row = [None]*(len(self.col_index_individual_name_ls)+1)	# extra 1 for the ref
-		data_row[0] = {'GT':refBase, 'DP':-1}
-		allele2count = {}
-		for individual_col_index, individual_name in self.col_index_individual_name_ls:
-			read_group = individual_name
-			if read_group not in self.sample_id2index:
-				self.sample_id2index[read_group] = len(self.sample_id2index)
-			
-			#coverage = read_group2coverage[read_group]
-			genotype_data = row[individual_col_index]
-			genotype_data_ls = genotype_data.split(':')
-			genotype_call_index = format_column_name2index.get('GT')
-			genotype_quality_index = format_column_name2index.get('GQ')
-			if genotype_quality_index is None:
-				genotype_quality_index = format_column_name2index.get('DP')
-			depth_index = format_column_name2index.get("DP")
-			#GL_index = format_column_name2index.get('GL')
-			if len(genotype_data_ls)<len(format_column_name2index):	#this genotype call is probably empty "./." due to no reads
-				continue
-			if depth_index is None or genotype_call_index is None:
-				sys.stderr.write("")
-				continue
-			#genotype_quality = genotype_data_ls[genotype_quality_index]
-			genotype_call = genotype_data_ls[genotype_call_index]
-			depth = int(genotype_data_ls[depth_index])
-			if depth<1:	#no read. samtools would still assign ref/ref to this individual
-				continue
-			#if depth>maxNoOfReads*coverage or depth<minNoOfReads*coverage:	#2011-3-29 skip. coverage too high or too low
-			#	continue
-			allele = 'NA'
-			callData = {'DP':depth}
-			if genotype_call=='0/1' or genotype_call =='1/0':	#heterozygous, the latter notation is never used though.
-				allele = '%s%s'%(refBase, altBase)
-				"""
-				GL_list = genotype_data_ls[GL_index]
-				GL_list = GL_list.split(',')
-				GL_list = map(float, GL_list)
-				GL = GL_list[1]
-				sndHighestGL = max([GL_list[0], GL_list[2]])
-				deltaGL = GL-sndHighestGL
-				"""
-				"""
-				
-				AD = genotype_data_ls[format_column_name2index.get('AD')]
-				AD = map(int, AD.split(','))
-				minorAlleleCoverage = min(AD)
-				majorAlleleCoverage = max(AD)
-				
-				if minorAlleleCoverage<=self.minorAlleleDepthUpperBoundCoeff*coverage and \
-						minorAlleleCoverage>=self.minorAlleleDepthLowerBoundCoeff*coverage and \
-						majorAlleleCoverage<=self.majorAlleleDepthUpperBoundCoeff*coverage:
-					DP4_ratio = float(AD[0])/AD[1]
-					allele = '%s%s'%(refBase, altBase)
-				"""
-			elif genotype_call=='./.':	#missing
-				allele = 'NA'
-			elif genotype_call =='1/1':
-				allele = '%s%s'%(altBase, altBase)
-			elif genotype_call =='0/0':
-				allele = '%s%s'%(refBase, refBase)
-			col_index = self.sample_id2index.get(read_group)
-			callData['GT'] = allele
-			data_row[col_index] = callData
-		self.chr = chr
-		self.pos = pos
-		self.locus = locus
-		self.quality = quality
-		self.info_tag2value = info_tag2value
-		self.data_row = data_row
-
+		self.chr = returnData.chr
+		self.pos = returnData.pos
+		self.locus_id = returnData.locus_id
+		self.quality = returnData.quality
+		self.info_tag2value = returnData.info_tag2value
+		self.data_row = returnData.data_row
+		self.alleleNumber2Base = returnData.alleleNumber2Base
+	
+	def getAAF(self):
+		"""
+		2011.12.16
+		"""
+		frequencyLs = self.info_tag2value.get("AF", self.info_tag2value.get("AF1", None))
+		if frequencyLs is not None:
+			frequencyLs = frequencyLs.split(',')	#if more than one alt alleles, AF is a string separated by ",". then choose the min
+			frequencyLs = map(float ,frequencyLs)
+			frequency = min(frequencyLs)
+		else:
+			frequency = -0.1
+		return frequency
+	
 class VCFFile(object):
 	__doc__ = __doc__
 	option_default_dict = {('inputFname', 1, ): [None, 'o', 1, 'a VCF input file.'],\
@@ -143,6 +211,7 @@ class VCFFile(object):
 						('depthLowerBoundCoeff', 1, float): [1/4., 'O', 1, 'depthLowerBoundCoeff*coverage = minimum read depth multiplier for one base'],\
 						('depthUpperBoundMultiSampleCoeff', 1, float): [3, 'N', 1, 'across n samples, ignore bases where read depth > coverageSum*depthUpperBoundCoeff*multiplier.'],\
 						('seqCoverageFname', 0, ): ['', 'q', 1, 'The sequence coverage file. tab/comma-delimited: individual_sequence.id coverage'],\
+						('minDepth', 1, float): [1, '', 1, 'minimum depth for a call to regarded as non-missing', ],\
 						('defaultCoverage', 1, float): [5, 'd', 1, 'default coverage when coverage is not available for a read group'],\
 						("site_type", 1, int): [1, 's', 1, '1: all sites, 2: variants only'],\
 						('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
@@ -206,6 +275,8 @@ class VCFFile(object):
 	
 	def parseFile(self):
 		"""
+		2011.11.30
+			bugfix: genotype_call could be ./.(missing) and depth=. (missing) in "./.:.:.:.:.".
 		2011-9-27
 			based on discoverFromVCF() from vervet/src/GenotypeCallByCoverage.py
 			
@@ -215,8 +286,6 @@ class VCFFile(object):
 		
 		sample_id2index = self.sample_id2index
 		locus_id2row_index = self.locus_id2row_index
-		sample_id2index['ref'] = 0	#ref is at column 0. "ref" must not be equal to any read_group.
-		read_group2coverage = {}	#2011-9-2
 		
 		"""
 		writer = csv.writer(open(outputFname, 'w'), delimiter='\t')
@@ -235,101 +304,19 @@ class VCFFile(object):
 		real_counter = 0
 		
 		for row in self.reader:
-			chr = row[0]
+			returnData = parseOneVCFRow(row, self.col_name2index, self.col_index_individual_name_ls, self.sample_id2index, \
+					minDepth=self.minDepth, dataEntryType=1)
 			
-			pos = int(row[1])
-			quality = row[5]
+			genotypeCall2Count = returnData.genotypeCall2Count
+			data_row = returnData.data_row
 			
-			info = row[7]
-			info_ls = info.split(';')
-			info_tag2value = {}
-			for info in info_ls:
-				try:
-					tag, value = info.split('=')
-				except:
-					#sys.stderr.write("Error in splitting %s by =.\n"%info)	###Error in splitting DS by =.
-					continue
-				info_tag2value[tag] = value
-			
-			current_locus = (chr, pos)
-			refBase = row[col_name2index['REF']]
-			altBase = row[col_name2index['ALT']]
+			current_locus = returnData.locus_id
 			if current_locus not in self.locus_id2data:
-				self.locus_id2data[current_locus] = info_tag2value
-				self.locus_id2data[current_locus]['REF'] = refBase
-				self.locus_id2data[current_locus]['ALT'] = altBase
+				self.locus_id2data[current_locus] = returnData.info_tag2value
+				self.locus_id2data[current_locus]['REF'] = returnData.refBase
+				self.locus_id2data[current_locus]['ALT'] = returnData.altBase
 			
-			format_column = row[col_name2index['FORMAT']]
-			format_column_ls = format_column.split(':')
-			format_column_name2index = getColName2IndexFromHeader(format_column_ls)
-			data_row = ['NA']*(len(individual_name2col_index)+1)	# extra 1 for the ref
-			data_row[0] = refBase
-			allele2count = {}
-			for individual_name, individual_col_index in individual_name2col_index.iteritems():
-				read_group = individual_name
-				if read_group not in sample_id2index:
-					sample_id2index[read_group] = len(sample_id2index)
-					self.sample_id_ls.append(read_group)
-				
-				#coverage = read_group2coverage[read_group]
-				genotype_data = row[individual_col_index]
-				genotype_data_ls = genotype_data.split(':')
-				genotype_call_index = format_column_name2index.get('GT')
-				genotype_quality_index = format_column_name2index.get('GQ')
-				if genotype_quality_index is None:
-					genotype_quality_index = format_column_name2index.get('DP')
-				depth_index = format_column_name2index.get("DP")
-				#GL_index = format_column_name2index.get('GL')
-				if len(genotype_data_ls)<len(format_column_name2index):	#this genotype call is probably empty "./." due to no reads
-					continue
-				if depth_index is None or genotype_call_index is None:
-					sys.stderr.write("")
-					continue
-				#genotype_quality = genotype_data_ls[genotype_quality_index]
-				genotype_call = genotype_data_ls[genotype_call_index]
-				depth = int(genotype_data_ls[depth_index])
-				if depth<1:	#no read. samtools would still assign ref/ref to this individual
-					continue
-				#if depth>maxNoOfReads*coverage or depth<minNoOfReads*coverage:	#2011-3-29 skip. coverage too high or too low
-				#	continue
-				allele = 'NA'
-				if genotype_call=='0/1' or genotype_call =='1/0':	#heterozygous, the latter notation is never used though.
-					allele = '%s%s'%(refBase, altBase)
-					"""
-					GL_list = genotype_data_ls[GL_index]
-					GL_list = GL_list.split(',')
-					GL_list = map(float, GL_list)
-					GL = GL_list[1]
-					sndHighestGL = max([GL_list[0], GL_list[2]])
-					deltaGL = GL-sndHighestGL
-					"""
-					"""
-					
-					AD = genotype_data_ls[format_column_name2index.get('AD')]
-					AD = map(int, AD.split(','))
-					minorAlleleCoverage = min(AD)
-					majorAlleleCoverage = max(AD)
-					
-					if minorAlleleCoverage<=self.minorAlleleDepthUpperBoundCoeff*coverage and \
-							minorAlleleCoverage>=self.minorAlleleDepthLowerBoundCoeff*coverage and \
-							majorAlleleCoverage<=self.majorAlleleDepthUpperBoundCoeff*coverage:
-						DP4_ratio = float(AD[0])/AD[1]
-						allele = '%s%s'%(refBase, altBase)
-					"""
-				elif genotype_call=='./.':	#missing
-					pass
-				elif genotype_call =='1/1':
-					allele = '%s%s'%(altBase, altBase)
-				elif genotype_call =='0/0':
-					allele = '%s%s'%(refBase, refBase)
-				col_index = sample_id2index.get(read_group)
-				data_row[col_index] = allele
-				if allele!='NA':
-					if allele not in allele2count:
-						allele2count[allele] = 0
-					allele2count[allele] += 1
-				
-			if len(allele2count)>self.site_type-1:	#whether polymorphic across samples or all sites in vcf
+			if len(genotypeCall2Count)>self.site_type-1:	#whether polymorphic across samples or all sites in vcf
 				real_counter += 1
 				locus_id2row_index[current_locus] = len(locus_id2row_index)
 				self.locus_id_ls.append(current_locus)
@@ -389,7 +376,8 @@ class VCFFile(object):
 		for row in self.reader:
 			yield VCFRecord(row, col_name2index=self.col_name2index, \
 						individual_name2col_index=self.individual_name2col_index, sample_id2index=self.sample_id2index,\
-						col_index_individual_name_ls=self.col_index_individual_name_ls)
+						col_index_individual_name_ls=self.col_index_individual_name_ls,\
+						minDepth=self.minDepth)
 	
 	def countHomoHetCallsForEachSampleFromVCF(self):
 		"""
@@ -403,7 +391,7 @@ class VCFFile(object):
 		no_of_total = 0.
 		minStart = None
 		for vcfRecord in self.parseIter():
-			locus = vcfRecord.locus
+			locus_id = vcfRecord.locus_id
 			chr = vcfRecord.chr
 			pos = vcfRecord.pos
 			pos = int(pos)
