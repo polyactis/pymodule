6b3fce28ff804bfe6dad5b3e4b6f09242c29288f
yuhuang <yuhuang@usc.edu>
2012-05-23 17:58:26 -0700

    change default of chrLength to 1 (was 0), added db_option_dict

--- pegasus/mapper/AbstractVCFMapper.py
+++ pegasus/mapper/AbstractVCFMapper.py
@@ -18,15 +18,26 @@ from pymodule import SNP
 
 class AbstractVCFMapper(object):
 	__doc__ = __doc__
-	option_default_dict = {('inputFname', 1, ): ['', 'i', 1, 'VCF input file. either plain vcf or gzipped is ok. could be unsorted.', ],\
+	db_option_dict = {
+					('drivername', 1,):['postgresql', 'v', 1, 'which type of database? mysql or postgresql', ],\
+					('hostname', 1, ): ['localhost', 'z', 1, 'hostname of the db server', ],\
+					('dbname', 1, ): ['vervetdb', 'd', 1, 'database name', ],\
+					('schema', 0, ): ['public', 'k', 1, 'database schema name', ],\
+					('db_user', 1, ): [None, 'u', 1, 'database username', ],\
+					('db_passwd', 1, ): [None, 'p', 1, 'database password', ],\
+					('port', 0, ):[None, '', 1, 'database port number'],\
+							}
+	option_default_dict = {
+						('inputFname', 1, ): ['', 'i', 1, 'VCF input file. either plain vcf or gzipped is ok. could be unsorted.', ],\
 						("home_path", 1, ): [os.path.expanduser("~"), 'e', 1, 'path to the home directory on the working nodes'],\
 						("chromosome", 0, ): [None, 'c', 1, 'chromosome name for these two VCF.'],\
-						("chrLength", 1, int): [0, 'l', 1, 'length of the reference used for the input VCF file.'],\
+						("chrLength", 1, int): [1, 'l', 1, 'length of the reference used for the input VCF file.'],\
 						('minDepth', 1, float): [0, 'm', 1, 'minimum depth for a call to regarded as non-missing', ],\
 						('outputFname', 0, ): [None, 'o', 1, 'output file'],\
 						('outputFnamePrefix', 0, ): [None, 'O', 1, 'output filename prefix (optional).'],\
 						('debug', 0, int):[0, 'b', 0, 'toggle debug mode'],\
-						('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']}
+						('report', 0, int):[0, 'r', 0, 'toggle report, more verbose stdout/stderr.']
+						}
 						#('bamListFname', 1, ): ['/tmp/bamFileList.txt', 'L', 1, 'The file contains path to each bam file, one file per line.'],\
 
 	def __init__(self,  **keywords):
