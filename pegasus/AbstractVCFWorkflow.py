617299efbb5636e6ae9be40c2b2e250f587e5db6
yuhuang <yuhuang@usc.edu>
2012-05-23 17:59:34 -0700

    registerAllInputFiles(): #2012.5.9 register the tbi file if it exists

--- pegasus/AbstractVCFWorkflow.py
+++ pegasus/AbstractVCFWorkflow.py
@@ -39,6 +39,8 @@ class AbstractVCFWorkflow(AbstractNGSWorkflow):
 	@classmethod
 	def registerAllInputFiles(cls, workflow, inputDir, input_site_handler=None, checkEmptyVCFByReading=False, pegasusFolderName=''):
 		"""
+		2012.5.9
+			register the tbi file if it exists
 		2012.3.1
 			moved from CalculateTrioInconsistencyPipeline.py
 		2012.1.9
@@ -51,16 +53,31 @@ class AbstractVCFWorkflow(AbstractNGSWorkflow):
 		fnameLs = os.listdir(inputDir)
 		counter = 0
 		real_counter = 0
+		previous_reported_real_counter = ''
 		for fname in fnameLs:
 			counter += 1
 			inputFname = os.path.join(inputDir, fname)
 			if NextGenSeq.isFileNameVCF(fname, includeIndelVCF=False) and \
 					not NextGenSeq.isVCFFileEmpty(inputFname, checkContent=checkEmptyVCFByReading):
 				real_counter += 1
-				inputF = File(os.path.join(pegasusFolderName, os.path.basename(inputFname)))
+				inputBaseFname = os.path.basename(inputFname)
+				inputF = File(os.path.join(pegasusFolderName, inputBaseFname))
 				inputF.addPFN(PFN("file://" + inputFname, input_site_handler))
 				inputF.abspath = inputFname
 				workflow.addFile(inputF)
-				returnData.jobDataLs.append(PassingData(vcfFile=inputF, jobLs=[]))
+				
+				tbi_F_absPath = "%s.tbi"%inputFname
+				if os.path.isfile(tbi_F_absPath):	#it exists
+					tbi_F = File(os.path.join(pegasusFolderName, "%s.tbi"%inputBaseFname))
+					tbi_F.addPFN(PFN("file://" + tbi_F_absPath, input_site_handler))
+					tbi_F.abspath = tbi_F_absPath
+					workflow.addFile(tbi_F)
+				else:
+					tbi_F = None
+				
+				returnData.jobDataLs.append(PassingData(vcfFile=inputF, jobLs=[], tbi_F=tbi_F))
+				if real_counter%200==0:
+					sys.stderr.write("%s%s"%('\x08'*len(previous_reported_real_counter), real_counter))
+					previous_reported_real_counter = repr(real_counter)
 		sys.stderr.write("%s non-empty out of %s files.\n"%(len(returnData.jobDataLs), counter))
 		return returnData
