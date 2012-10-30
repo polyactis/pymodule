a88ae3dae467727e45e06e7e2dbfe3aefa22f019
crocea <crocea@uclaOffice>
2012-10-30 10:35:11 -0700

    add these arguments: 		('maxDist', 0, int): [None, '', 1, 'if given, pairs beyond this distance are tossed.', ],\ 		('minDist', 0, int): [None, '', 1, 'if given, pairs below this distance are tossed.', ],\ 		('movingAverageType', 0, int): [2, '', 1, '1: median within each step, 2: mean, 3: fraction that is >0.8.', ],\

--- plot/PlotLD.py
+++ plot/PlotLD.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 import matplotlib; matplotlib.use("Agg")	#to disable pop-up requirement
 import csv
 from pymodule import ProcessOptions, getListOutOfStr, PassingData, getColName2IndexFromHeader, figureOutDelimiter
-from pymodule import yh_matplotlib, GenomeDB
+from pymodule import yh_matplotlib, GenomeDB, statistics
 import numpy, random, pylab
 from AbstractPlot import AbstractPlot
 from pymodule.AbstractMatrixFileWalker import AbstractMatrixFileWalker
@@ -39,8 +39,13 @@ class PlotLD(AbstractPlot):
 			('chrColumnHeader', 1, ): ['CHR', 'C', 1, 'label of the chromosome column', ],\
 			('minChrLength', 1, int): [1000000, 'L', 1, 'minimum chromosome length for one chromosome to be included', ],\
 			('pos2ColumnHeader', 1, ): ['POS2', 'u', 1, 'label of the 2nd position column, xColumnHeader is the 1st position column', ],\
+			('maxDist', 0, int): [None, '', 1, 'if given, pairs beyond this distance are tossed.', ],\
+			('minDist', 0, int): [None, '', 1, 'if given, pairs below this distance are tossed.', ],\
+			('movingAverageType', 0, int): [2, '', 1, '1: median within each step, 2: mean, 3: fraction that is >0.8.', ],\
 #			('fitCurve', 0, ): [0, '', 0, 'toggle to fit an exponential decay function to the data', ],\
 			})
+	option_default_dict[('missingDataNotation', 0, )][0] = '-nan'
+	
 	#option_default_dict.pop(('outputFname', 1, ))
 	
 	"""
@@ -59,21 +64,32 @@ class PlotLD(AbstractPlot):
 	
 	def plot(self, x_ls=None, y_ls=None, pdata=None):
 		"""
-		2012.9.6 overwrite the ancetral function
+		2012.9.6 overwrite the ancestral function
 		2011-9-30
 			get called by the end of fileWalker() for each inputFname.
 		"""
-		pylab.plot(x_ls, y_ls, self.formatString, alpha=0.3)
+		#pylab.plot(x_ls, y_ls, self.formatString, alpha=0.3)
 		#if self.fitCurve:
+		movingAverageData = statistics.movingAverage(listOfList=[x_ls, y_ls], no_of_steps=20, needReorderData=True, \
+										reduceType=self.movingAverageType, minValueForFraction=0.8)
+		n_x_ls = movingAverageData.listOfList[0]
+		n_y_ls = movingAverageData.listOfList[1]
+		pylab.plot(n_x_ls, n_y_ls, self.formatString)
+		pylab.ylim(ymin=0)
+		#pylab.ylim(ymax=1.0)
+		#splineFitData = statistics.splineFit(x_ls=x_ls, y_ls=y_ls, no_of_steps=100, needReorderData=True)
+		#pylab.plot(splineFitData.x_ls, splineFitData.y_ls)
+		"""
 		x_ar = numpy.array(x_ls, numpy.float)
 		y_ar = numpy.array(y_ls, numpy.float)
 		#sort x_ar and y_ar must be in the order of x_ar
 		indexOfOrderList = numpy.argsort(x_ar)
 		x_ar = x_ar[indexOfOrderList]
 		y_ar = y_ar[indexOfOrderList]
-		from Bio.Statistics import lowess
-		lowessY = lowess.lowess(x_ar, y_ar)
-		pylab.plot(x_ar, lowessY)
+		#from Bio.Statistics import lowess
+		#lowessY = lowess.lowess(x_ar, y_ar)
+		"""
+		
 		"""
 		### error from this code:
 		#	Warning: overflow encountered in multiply
@@ -93,6 +109,8 @@ class PlotLD(AbstractPlot):
 	
 	def processRow(self, row=None, pdata=None):
 		"""
+		2012.10.25
+			handle self.minDist and self.maxDist, make sure pairs within this distance.
 		2012.8.31 skip missing data via self.missingDataNotation
 		2012.8.18
 		"""
@@ -116,14 +134,18 @@ class PlotLD(AbstractPlot):
 						return
 			if whichColumn is not None and pos1_index is not None and pos2_index is not None:
 				yValue = row[whichColumn]
-				if yValue!=self.missingDataNotation:
+				if yValue not in self.missingDataNotation:
 					pos1 = int(float(row[pos1_index]))
 					pos2 = int(float(row[pos2_index]))
 					xValue = abs(pos2-pos1)
+					if self.minDist and xValue<self.minDist:
+						return
+					if self.maxDist and xValue>self.maxDist:
+						return
 					x_ls.append(xValue)
 					yValue = self.handleYValue(yValue)
 					y_ls.append(yValue)
-	
+		return 1
 	
 if __name__ == '__main__':
 	main_class = PlotLD
