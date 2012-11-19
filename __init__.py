c1b3149cfa1252fa764f44a58f5796423047a267
crocea <crocea@uclaOffice>
2012-11-19 12:30:38 -0800

    move CNV.py from Genome/ to io/

--- __init__.py
+++ __init__.py
@@ -6,20 +6,34 @@ sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 from ProcessOptions import ProcessOptions, generate_program_doc, process_options, process_function_arguments, turn_option_default_dict2argument_default_dict
-from io.SNP import write_data_matrix, read_data, SNPData, GenomeWideResults, GenomeWideResult, DataObject, getGenomeWideResultFromFile,\
-	nt2number, number2nt, SNPInfo, number2single_char_nt
-from io.TwoSNPData import TwoSNPData, QualityControl
 from utils import PassingData, dict_map, importNumericArray, figureOutDelimiter, get_gene_symbol2gene_id_set, \
 	FigureOutTaxID, getColName2IndexFromHeader, getListOutOfStr, runLocalCommand, getGeneIDSetGivenAccVer, calGreatCircleDistance
-
-from Genome import GeneModel, CNV
-from Genome.CNV import CNVCompare, CNVSegmentBinarySearchTreeKey, get_overlap_ratio
 
 from plot import yh_matplotlib, yh_matplotlib_artists
 from plot.yh_matplotlib import assignMatPlotlibHueColorToLs, drawName2FCLegend
 
+from db import GenomeDB
+
+from algorithm import pca_module
+from algorithm.PCA import PCA
+from algorithm.RBTree import RBTree, RBDict, RBTreeIter, RBList, RBNode
+from algorithm.BinarySearchTree import binary_tree
+
 #2012.11.14
+from io import SNP
+from io import NextGenSeq
 from io.MatrixFile import MatrixFile
+from io.SNP import write_data_matrix, read_data, SNPData, GenomeWideResults, GenomeWideResult, DataObject, getGenomeWideResultFromFile,\
+	nt2number, number2nt, SNPInfo, number2single_char_nt
+from io.TwoSNPData import TwoSNPData, QualityControl
+from io.HDF5MatrixFile import HDF5MatrixFile 
+from io.BamFile import BamFile
+from io.VCFFile import VCFFile, VCFRecord
+from io.CNV import CNVCompare, CNVSegmentBinarySearchTreeKey, get_overlap_ratio
+
+from Genome import GeneModel
+
+
 #2012.11.18
 from AbstractDBInteractingClass import AbstractDBInteractingClass
 from pegasus.AbstractWorkflow import AbstractWorkflow
@@ -27,16 +41,3 @@ from pegasus.mapper.AbstractMapper import AbstractMapper
 from pegasus.mapper.AbstractVCFMapper import AbstractVCFMapper
 from pegasus.mapper.AbstractDBInteractingJob import AbstractDBInteractingJob
 from pegasus import yh_pegasus
-
-from io.HDF5MatrixFile import HDF5MatrixFile 
-from io.BamFile import BamFile
-from io.VCFFile import VCFFile, VCFRecord
-from io import SNP
-from io import NextGenSeq
-
-from db import GenomeDB
-
-from algorithm.PCA import PCA
-from algorithm import pca_module
-from algorithm.RBTree import RBTree, RBDict, RBTreeIter, RBList, RBNode
-from algorithm.BinarySearchTree import binary_tree
