80ab90a87f88b3adade9f561751e9181fa012f78
Yu Huang <yu.huang@uclaOffice>
2013-02-16 16:21:27 -0800

    stop wrapping the status-checking inside a while-sleep loop

--- pegasus/checkCondorNodeProperty.sh
+++ pegasus/checkCondorNodeProperty.sh
@@ -8,11 +8,11 @@ then
 fi
 
 #watch -d -n 12 "condor_status |tail -n $noOfLines"
-while [ 0==0 ];do
+#while [ 0==0 ];do
 	fname1=/tmp/condorMachine
 	fname2=/tmp/condorSSHDBTunnel
 	condor_status -long|grep "^Machine " > $fname1
 	condor_status -long|grep "^$property" > $fname2
 	paste $fname1 $fname2|less
-	sleep 5
-done
+	#sleep 5
+#done
