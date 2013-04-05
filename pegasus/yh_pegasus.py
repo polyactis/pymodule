cb3c4a6802467a26fc8466bcb8523894fc044d6c
Yu Huang <yu.huang@uclaOffice>
2013-04-05 08:28:16 -0700

    cosmetic

--- pegasus/yh_pegasus.py
+++ pegasus/yh_pegasus.py
@@ -7,6 +7,7 @@ import os, sys
 sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 from Pegasus.DAX3 import *
+from pymodule.utils import PassingData
 
 def addMkDirJob(workflow=None, mkdir=None, outputDir=None, namespace=None, version=None,\
 			parentJobLs=None, extraDependentInputLs=None):
@@ -41,15 +42,18 @@ def addMkDirJob(workflow=None, mkdir=None, outputDir=None, namespace=None, versi
 
 def registerRefFastaFile(workflow=None, refFastaFname=None, registerAffiliateFiles=True, input_site_handler='local',\
 						checkAffiliateFileExistence=True, addPicardDictFile=True,\
-						affiliateFilenameSuffixLs=['dict', 'fai', 'amb', 'ann', 'bwt', 'pac', 'sa', 'rbwt', 'rpac', 'rsa', \
-						'stidx', 'sthash'], folderName=""):
+						affiliateFilenameSuffixLs=['fai', 'amb', 'ann', 'bwt', 'pac', 'sa', 'rbwt', 'rpac', 'rsa', \
+						'stidx', 'sthash'], folderName="reference"):
 	"""
+	suffix here doesn't include ".".
+	2013.3.26 added refSAMtoolsFastaIndexF, refPicardFastaDictF into returnData
+	2013.3.20 deduce needBWARefIndexJob, needSAMtoolsFastaIndexJob, needPicardFastaDictJob, needStampyRefIndexJob from missing suffixes
 	2010.10.10 added argument folderName
 	2012.5.23
-		add an argument "addPicardDictFile" to offer user option to exclude this file (i.e. registerBlastNucleotideDatabaseFile)
+		add an argument "addPicardDictFile" to offer user option to exclude this file (i.e. in registerBlastNucleotideDatabaseFile)
 	2012.2.24
-		dict is via picard
-		fai is via "samtools faidx" (index reference)
+		dict is via picard, also required for GATK
+		fai is via "samtools faidx" (index reference). also required for GATK
 		amb', 'ann', 'bwt', 'pac', 'sa', 'rbwt', 'rpac', 'rsa' are all bwa index.
 		stidx is stampy index.
 		sthash is stampy hash.
@@ -59,25 +63,39 @@ def registerRefFastaFile(workflow=None, refFastaFname=None, registerAffiliateFil
 		if needAffiliatedFiles,
 			all other files, with suffix in affiliateFilenameSuffixLs, will be registered (symlinked or copied) as well.
 	"""
-	refFastaFList = []
+	returnData = PassingData(refFastaFList = [], needBWARefIndexJob=False, needSAMtoolsFastaIndexJob=False, \
+							needPicardFastaDictJob=False, needStampyRefIndexJob=False, needBlastMakeDBJob=False,\
+							refPicardFastaDictF=None, refSAMtoolsFastaIndexF=None)
+	missingSuffixSet = set()	#2013.3.20
+	
 	if registerAffiliateFiles:
 		refFastaF = File(os.path.join(folderName, os.path.basename(refFastaFname)))	#use relative path, otherwise, it'll go to absolute path
 		# Add it into replica only when needed.
 		refFastaF.addPFN(PFN("file://" + refFastaFname, input_site_handler))
 		workflow.addFile(refFastaF)
-		refFastaFList.append(refFastaF)
+		returnData.refFastaFList.append(refFastaF)
 		# If it's not needed, assume the index is done and all relevant files are in absolute path.
 		# and no replica transfer
 		
 		#add extra affiliated files
-		pathToFileLs = []
+		suffix2PathToFileLs = {}
 		if addPicardDictFile:	#2012.5.23
-			dictFname = '%s.dict'%(os.path.splitext(refFastaFname)[0])	#remove ".fasta" from refFastaFname
-			pathToFileLs.append(dictFname)
+			picardDictSuffix = 'dict'
+			pathToFile = '%s.%s'%(os.path.splitext(refFastaFname)[0], picardDictSuffix)	#remove ".fasta" from refFastaFname
+			if checkAffiliateFileExistence and not os.path.isfile(pathToFile):
+				sys.stderr.write("Warning: %s don't exist or not a file on file system. skip registration.\n"%(pathToFile))
+				missingSuffixSet.add(picardDictSuffix)
+				#suffix2PathToFileLs.append(pathToFile)
+			else:
+				suffix2PathToFileLs[picardDictSuffix] = pathToFile
 		for suffix in affiliateFilenameSuffixLs:
 			pathToFile = '%s.%s'%(refFastaFname, suffix)
-			pathToFileLs.append(pathToFile)
-		for pathToFile in pathToFileLs:
+			if checkAffiliateFileExistence and not os.path.isfile(pathToFile):
+				sys.stderr.write("Warning: %s don't exist or not a file on file system. skip registration.\n"%(pathToFile))
+				missingSuffixSet.add(suffix)
+				continue
+			suffix2PathToFileLs[suffix]= pathToFile
+		for suffix, pathToFile in suffix2PathToFileLs.iteritems():
 			if checkAffiliateFileExistence and not os.path.isfile(pathToFile):
 				sys.stderr.write("Warning: %s don't exist or not a file on file system. skip registration.\n"%(pathToFile))
 				continue
@@ -85,27 +103,43 @@ def registerRefFastaFile(workflow=None, refFastaFname=None, registerAffiliateFil
 			#use relative path, otherwise, it'll go to absolute path
 			affiliateF.addPFN(PFN("file://" + pathToFile, input_site_handler))
 			workflow.addFile(affiliateF)
-			refFastaFList.append(affiliateF)
+			returnData.refFastaFList.append(affiliateF)
+			
+			if suffix=='dict':	#2013.3.26
+				returnData.refPicardFastaDictF = affiliateF
+			elif suffix=='fai':
+				returnData.refSAMtoolsFastaIndexF = affiliateF
 	else:
 		refFastaF = File(os.path.join(folderName, os.path.basename(refFastaFname)))
-		refFastaFList.append(refFastaF)
-	return refFastaFList
+		returnData.refFastaFList.append(refFastaF)
+	if 'bwt' in missingSuffixSet or 'pac' in missingSuffixSet:
+		returnData.needBWARefIndexJob = True
+	if 'fai' in missingSuffixSet:
+		returnData.needSAMtoolsFastaIndexJob = True
+		returnData.needPicardFastaDictJob = True
+	if 'stidx' in missingSuffixSet or 'sthash' in missingSuffixSet:
+		returnData.needStampyRefIndexJob = True
+	if 'dict' in missingSuffixSet:
+		returnData.needPicardFastaDictJob = True
+	if 'nin' in missingSuffixSet or 'nhr' in missingSuffixSet or 'nsq' in missingSuffixSet:
+		returnData.needBlastMakeDBJob = True
+	return returnData
 
-def setJobToProperMemoryRequirement(job, job_max_memory=500, no_of_cpus=1, max_walltime=180, sshDBTunnel=0):
+def setJobToProperMemoryRequirement(job, job_max_memory=500, no_of_cpus=1, walltime=180, sshDBTunnel=0):
 	"""
 	2012.8.15
-		increase default max_walltime to 180
+		increase default walltime to 180
 	2012.4.16
 		add argument sshDBTunnel.
 			=1: this job needs a ssh tunnel to access psql db on dl324b-1.
 			=anything else: no need for that.
 	2011-11-23
-		set max_walltime default to 120 minutes (2 hours)
+		set walltime default to 120 minutes (2 hours)
 	2011-11-16
 		add more requirements
 	2011-11-11
 		job_max_memory is in MB.
-		max_walltime is in minutes.
+		walltime is in minutes.
 	"""
 	job.addProfile(Profile(Namespace.GLOBUS, key="maxmemory", value="%s"%(job_max_memory)))
 	job.addProfile(Profile(Namespace.CONDOR, key="request_memory", value="%s"%(job_max_memory)))	#for dynamic slots
@@ -117,10 +151,11 @@ def setJobToProperMemoryRequirement(job, job_max_memory=500, no_of_cpus=1, max_w
 	if no_of_cpus is not None:
 		job.addProfile(Profile(Namespace.CONDOR, key="request_cpus", value="%s"%(no_of_cpus)) )	#for dynamic slots
 	
-	if max_walltime is not None:
-		job.addProfile(Profile(Namespace.GLOBUS, key="maxwalltime", value="%s"%(max_walltime)) )
+	if walltime is not None:
+		#2013.3.21 scale walltime according to clusters_size
+		job.addProfile(Profile(Namespace.GLOBUS, key="maxwalltime", value="%s"%(walltime)) )
 		#TimeToLive is in seconds
-		condorJobRequirementLs.append("(Target.TimeToLive>=%s)"%(int(max_walltime)*60) )
+		condorJobRequirementLs.append("(Target.TimeToLive>=%s)"%(int(walltime)*60) )
 	#key='requirements' could only be added once for the condor profile
 	job.addProfile(Profile(Namespace.CONDOR, key="requirements", value=" && ".join(condorJobRequirementLs) ))
 
@@ -154,3 +189,14 @@ def getAbsPathOutOfFile(file):
 		call getAbsPathOutOfExecutable
 	"""
 	return getAbsPathOutOfExecutable(file)
+
+def getExecutableClustersSize(executable=None):
+	"""
+	2013.03.21, default is None
+	"""
+	clusters_size = None
+	clusteringProf = Profile(Namespace.PEGASUS, key="clusters.size", value="1")
+	for profile in executable.profiles:
+		if clusteringProf.__hash__() == profile.__hash__():	#__hash__ only involves namespace + key 
+			clusters_size = profile.value
+	return clusters_size
