cd5d870d9e0f6c3856f9cebe297c1a70ffaf9a4b
Yu Huang <yu.huang@uclaOffice>
2013-09-05 09:25:49 -0700

    cosmetic

--- pegasus/mapper/extractor/ExtractInfoFromVCF.py
+++ pegasus/mapper/extractor/ExtractInfoFromVCF.py
@@ -16,7 +16,7 @@ __doc__ = __doc__%(sys.argv[0], sys.argv[0], sys.argv[0])
 sys.path.insert(0, os.path.expanduser('~/lib/python'))
 sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
-import cStringIO, re, csv
+import csv
 from pymodule import ProcessOptions, figureOutDelimiter
 from pymodule.utils import sortCMPBySecondTupleValue
 from pymodule.yhio.VCFFile import VCFFile
