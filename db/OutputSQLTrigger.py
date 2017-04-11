303aa21bf54f1e5d2b29b83e056de54abf30f825
System User <fanxp@node100.simm.ac.cn>
2017-04-11 10:55:47 +0800

    modify OutputSQLTrigger.py: delete syntax for 32bit system lib.

--- db/OutputSQLTrigger.py
+++ db/OutputSQLTrigger.py
@@ -13,13 +13,8 @@ Description:
 """
 
 import sys, os, math
-bit_number = math.log(sys.maxint)/math.log(2)
-if bit_number>40:       #64bit
-	sys.path.insert(0, os.path.expanduser('~/lib64/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script64')))
-else:   #32bit
-	sys.path.insert(0, os.path.expanduser('~/lib/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
+sys.path.insert(0, os.path.expanduser('~/lib/python'))
+sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 mysql_trigger = """
 DELIMITER |
