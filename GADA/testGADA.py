62ddcf9e5bea6d57b71d801a2e0298a303c216b8
luozhihui <luozhihui@ubuntu.com>
2017-11-17 13:33:01 +0800

    remove 64bit importing

--- GADA/testGADA.py
+++ GADA/testGADA.py
@@ -15,13 +15,9 @@ Description:
 """
 
 import sys, os, math
-bit_number = math.log(sys.maxint)/math.log(2)
-if bit_number>40:       #64bit
-	sys.path.insert(0, os.path.expanduser('~/lib64/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script64')))
-else:   #32bit
-	sys.path.insert(0, os.path.expanduser('~/lib/python'))
-	sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
+#bit_number = math.log(sys.maxint)/math.log(2)
+sys.path.insert(0, os.path.expanduser('~/lib/python'))
+sys.path.insert(0, os.path.join(os.path.expanduser('~/script')))
 
 
 from pymodule import MatrixFile
