1452460f5bc0f689135ac7f6cfe51c4653d8ca0b
Yu Huang@neon <polyactis@gmail.com>
2020-05-12 09:33:16 +0800

    cosmetic

--- mapper/computer/CountFastqReadBaseCount.py
+++ mapper/computer/CountFastqReadBaseCount.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 2012.3.14
-	count the number of reads/bases of fastq or fasta input file.
+    count the number of reads/bases of fastq or fasta input file.
 """
 
 import sys, os
@@ -11,38 +11,38 @@ from palos.mapper.AbstractMapper import AbstractMapper
 from palos import ngs
 
 class CountFastqReadBaseCount(AbstractMapper):
-	__doc__ = __doc__
-	option_default_dict = AbstractMapper.option_default_dict.copy()
-	option_default_dict.update({
-							('isq_id', 0, int): [0, 'q', 1, 'IndividualSequence.id'],\
-							('isqf_id', 0, int): [0, 'f', 1, 'IndividualSequenceFile.id if applicable'],\
-							})
-	def __init__(self, inputFnameLs, **keywords):
-		"""
-		"""
-		AbstractMapper.__init__(self, **keywords)
-		self.inputFnameLs = inputFnameLs
-		#2012.3.19 not used
-	
-	def run(self):
-		"""
-		"""
-		
-		if self.debug:
-			import pdb
-			pdb.set_trace()
-		
-		baseCountData = ngs.getReadBaseCount(self.inputFname)
-		
-		writer = csv.writer(open(self.outputFname, 'w'), delimiter='\t')
-		header = ['isq_id', 'isqf_id', 'read_count', 'base_count']
-		writer.writerow(header)
-		data_row = [self.isq_id, self.isqf_id, baseCountData.read_count, baseCountData.base_count]
-		writer.writerow(data_row)
-		del writer
-	
+    __doc__ = __doc__
+    option_default_dict = AbstractMapper.option_default_dict.copy()
+    option_default_dict.update({
+        ('isq_id', 0, int): [0, 'q', 1, 'IndividualSequence.id'],\
+        ('isqf_id', 0, int): [0, 'f', 1, 'IndividualSequenceFile.id if applicable'],
+    })
+    def __init__(self, inputFnameLs, **keywords):
+        """
+        """
+        AbstractMapper.__init__(self, **keywords)
+        self.inputFnameLs = inputFnameLs
+    
+    def run(self):
+        """
+        """
+        
+        if self.debug:
+            import pdb
+            pdb.set_trace()
+        
+        pdata = ngs.getReadBaseCount(self.inputFname)
+        
+        writer = csv.writer(open(self.outputFname, 'w'), delimiter='\t')
+        header = ['isq_id', 'isqf_id', 'read_count', 'base_count']
+        writer.writerow(header)
+        data_row = [self.isq_id, self.isqf_id, pdata.read_count, pdata.base_count]
+        writer.writerow(data_row)
+        del writer
+    
 if __name__ == '__main__':
-	main_class = CountFastqReadBaseCount
-	po = ProcessOptions(sys.argv, main_class.option_default_dict, error_doc=main_class.__doc__)
-	instance = main_class(po.arguments, **po.long_option2value)
-	instance.run()
+    main_class = CountFastqReadBaseCount
+    po = ProcessOptions(sys.argv, main_class.option_default_dict,
+        error_doc=main_class.__doc__)
+    instance = main_class(po.arguments, **po.long_option2value)
+    instance.run()
