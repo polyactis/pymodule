5d510f492822a18ad74641b1e67ac1f126f383e7
Yu Huang@neon <polyactis@gmail.com>
2020-05-21 15:21:36 +0800

    v 0.1.16

--- palos/__init__.py
+++ palos/__init__.py
@@ -3,7 +3,7 @@ Palos is a Python3 module developed and used by the yfish group, http://www.yfis
 It contains code related to bioinformatics projects focusing on next-generation sequencing data, 
 population genetics, genome-wide association studies, pedigree genetics, etc.
 """
-version='0.1.15'
+version='0.1.16'
 from . ProcessOptions import ProcessOptions, generate_program_doc, process_options, \
     process_function_arguments, turn_option_default_dict2argument_default_dict
 from . utils import PassingData, PassingDataList, dict_map, importNumericArray, \
